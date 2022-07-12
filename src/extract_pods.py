5ac8aebccdd56456e3755058c9ff522d71a7d5ef
Shay Ulmer <shayulmer@gmail.com>
2022-07-12 14:31:10 +0300

    fix pod state

--- src/extract_pods.py
+++ src/extract_pods.py
@@ -11,15 +11,25 @@ def main():
     pods = v1.list_pod_for_all_namespaces(watch=False)
     pods_details_list = []
     for pod in pods.items:
+        status = get_pod_status(v1,pod)
         dict = {
             "name": pod.metadata.name,
             "namespace": pod.metadata.namespace,
             "ip": pod.status.pod_ip,
             "node": pod.spec.node_name,
-            "status": pod.status.phase
+            "status": status
         }
         pods_details_list.append(dict)
     return (pods_details_list)
+
+def get_pod_status(v1,pod):
+    status = pod.status.phase
+    for container_status in pod.status.container_statuses:
+        if container_status.started is False or container_status.ready is False:
+            waiting_state = container_status.state.waiting
+            if waiting_state is not None:
+                status = waiting_state.reason
+    return status
 
 @app.route('/')
 def hello_world():
